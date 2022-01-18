4c3c20077d725557e78ef86bb6782166f77ad6ed
hyuk <dlsgur0906@gmail.com>
2022-01-18 17:51:52 +0900

    header작성 중

---
+++ src/include/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className='header'>
+            <h1>SIGNATURE BED</h1>
+            <ul>
+                <li>HOME</li>
+                <li>COLLECTION</li>
+                <li>PRODUCT</li>
+                <li>STORE</li>
+                <li>ABOUT</li>
+                <li>STORY</li>
+                <li>NOTICE</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
