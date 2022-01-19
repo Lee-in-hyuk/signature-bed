20553361557ccc5637d4426774231fdbf8145c92
hyuk <dlsgur0906@gmail.com>
2022-01-19 14:07:13 +0900

    헤더, 컨테이너1까지 작업 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './include/Header';
+import Main from './main/Main';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
     </div>
   );
 }
--- src/main/Main.js
+++ src/main/Main.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Carousel } from "antd";
+import 'antd/dist/antd.min.css';
+import { ImYoutube } from 'react-icons/im';
+import './main.scss';
 
-function Main(props) {
+function Main() {
     return (
-        <div>
-            
+        <div id='wrap'>
+            <div className='slide-image'>
+                <Carousel autoplay={true} autoplaySpeed={3000} effect="fade">
+                    <div><img src='signature-bed-image/slide-image01.jpg' alt='bed1'/></div>
+                    <div><img src='signature-bed-image/slide-image02.jpg' alt='bed2'/></div>
+                    <div><img src='signature-bed-image/slide-image03.jpg' alt='bed3'/></div>
+                </Carousel>
+            </div>
+            <div id='container01'>
+                <div id='con_text' className='container1'>
+                    <h2>The best foam</h2>
+                    <h3>최상의 폼</h3>
+                    <h2>Optimal layer</h2>
+                    <h3>최적의 비율</h3>
+                    <h2>Perfect sleep</h2>
+                    <h3>완벽한 숙면</h3>
+                </div>
+                <div id='con_img' className='container1'>
+                    <img src='signature-bed-image/container1.jpg' alt='video'/>
+                    <a href='https://youtu.be/3xfTtMlKcWs'><span><ImYoutube/></span></a>
+                    {/* <video src='https://youtu.be/3xfTtMlKcWs' width="600px" height="400px" controls><span><ImYoutube/></span></video> */}
+                </div>
+            </div>
         </div>
     );
 }
